e4dad943c2a7e4f22292edaf56b28e5f20ee4464
Théo Marinho <thmarinho@outlook.fr>
2024-08-25 11:29:25 +0200

    [fix] build

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://91.134.88.126:1337";
+export const API_URL = "https://cpool-api.thmarinho.dev";
 export const EMAILJS_SERVICE = ""
 export const EMAILJS_TEMPLATE = ""
 export const EMAILJS_KEY = ""
