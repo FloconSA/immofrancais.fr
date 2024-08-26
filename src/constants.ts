762f2789acae439790c05bbd49a6487c5fbaa31a
FloconSA <fcarre.if@gmail.com>
2024-08-26 11:29:11 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://cpool-api.thmarinho.dev";
+export const API_URL = "https://api.immo-francais.fr";
 export const EMAILJS_SERVICE = "service_m3d9mcr"
 export const EMAILJS_TEMPLATE = "template_7v34sjv"
 export const EMAILJS_KEY = "_SiitZqXVWpGq1Oj6"
