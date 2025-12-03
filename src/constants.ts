c125c602fee9271371579f75299b4e6441b3e27f
FloconSA <fcarre.if@gmail.com>
2025-12-03 23:22:12 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "backend-immo-production-7c35.up.railway.app";
+export const API_URL = "https://backend-immo-production-7c35.up.railway.app";
 export const EMAILJS_SERVICE = "service_m3d9mcr"
 export const EMAILJS_TEMPLATE = "template_7v34sjv"
 export const EMAILJS_KEY = "_SiitZqXVWpGq1Oj6"
